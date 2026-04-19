cfe92923be0ba7ff1b55b99a0285f5fc7de03da3
FuzzyZver <12andzver@gmail.com>
2026-04-19 16:26:34 +0300

    PSX camera effect

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Way to Home
 - Unity version: Unity 6000.3.8f1
 - Active game object:
-  - Name: PlayerCamera
+  - Name: GameManager
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Game/Scripts/Systems/InitSystem.cs
+++ Assets/Game/Scripts/Systems/InitSystem.cs
@@ -6,5 +6,12 @@ public class InitSystem: Injects, IEcsPreInitSystem
     public void PreInit()
     {
         SceneData.Player.Init(EcsWorld);
+
+        //Render fog init
+        RenderSettings.fog = true;
+        RenderSettings.fogMode = FogMode.Exponential;
+        RenderSettings.fogDensity = 0.2f;
+        RenderSettings.fogColor = Color.gray;
+        Application.targetFrameRate = 30;
     }
 }
